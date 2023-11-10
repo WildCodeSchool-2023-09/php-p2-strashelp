0b80bfffabc2bab38eda9101072a4eb25b3e64f2
William-lh <lherm.william@gmail.com>
2023-11-10 23:50:32 +0100

    annonce back et intégrétation twig grump1

---
+++ src/Model/AnnoncesManager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model;
+
+class AnnoncesManager extends AbstractManager
+{
+    public const TABLE = 'ads';
+
+    public function selectAll(string $orderBy = 'published_date', string $direction = 'DESC'): array
+    {
+        $query = 'SELECT * FROM ' . static::TABLE;
+        if ($orderBy) {
+            $query .= ' ORDER BY ' . $orderBy . ' ' . $direction . ' LIMIT 9';
+        }
+        return $this->pdo->query($query)->fetchAll();
+    }
+}
