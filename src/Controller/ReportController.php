16744861afdf5e13ee83127231ce63812fa62da5
Elise <elise.chaldjian@gmail.com>
2023-11-12 01:14:53 +0100

    Avancée reportad

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -9,8 +9,8 @@ class ReportController extends AbstractController
     public function showReports()
     {
         $reportManager = new ReportManager();
-        $reports = $reportManager->selectAll('title');
+        $reports = $reportManager->selectAll('report_reason');
 
-        return $this->twig->render('Item/index.html.twig', ['reports' => $reports]);
+        return $this->twig->render('Home/index.html.twig', ['reports' => $reports]);
     }
 }
