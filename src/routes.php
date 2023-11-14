f861f2fe65368033cb783831f6980d54a57be87d
William-lh <lherm.william@gmail.com>
2023-11-14 08:45:15 +0100

    contactform finalisation 7

--- src/routes.php
+++ src/routes.php
@@ -18,5 +18,5 @@ return [
     'inscription' => ['InscriptionController','inscription',],
     'connexion' => ['ConnexionController','connexion',],
     'annonces' => ['AnnoncesController','annonces',],
-    'mentionlegales' => ['MentionLegalesController','mentionlegales',],    
+    'mentionlegales' => ['MentionLegalesController','mentionlegales',],
 ];
