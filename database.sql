e32f49aecc4f3e434f7d6b0a2ba6439dbb3337c0
Julien PRIVAT <contact@julien-privat.com>
2023-11-22 13:03:54 +0100

    Changements

--- database.sql
+++ database.sql
@@ -1,96 +1,144 @@
 -- phpMyAdmin SQL Dump
+
 -- version 4.5.4.1deb2ubuntu2
+
 -- http://www.phpmyadmin.net
+
 --
+
 -- Client :  localhost
+
 -- Généré le :  Jeu 26 Octobre 2017 à 13:53
+
 -- Version du serveur :  5.7.19-0ubuntu0.16.04.1
+
 -- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 SET time_zone = "+00:00";
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+
+;
+
+/*!40101 SET NAMES utf8mb4 */
+
+;
 
 --
+
 -- Base de données :  `simple-mvc`
+
 --
 
 -- --------------------------------------------------------
 
-CREATE DATABASE `StrasHelp`;
-USE `StrasHelp`;
+CREATE TABLE
+    `user` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `firstname` VARCHAR(100) NOT NULL,
+        `lastname` VARCHAR(100) NOT NULL,
+        `username` VARCHAR(100) NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        `email` VARCHAR(150) NOT NULL,
+        `phone_number` INT(11) NOT NULL,
+        `birthdate` date NOT NULL,
+        `localisation` VARCHAR(150) NOT NULL,
+        `is_admin` bool NULL
+    );
 
-CREATE TABLE `user` (
-`id` INT PRIMARY KEY AUTO_INCREMENT  NOT NULL,
-`firstname` VARCHAR(100) NOT NULL,
-`lastname` VARCHAR(100) NOT NULL,
-`username`VARCHAR(100) NOT NULL,
-`password` VARCHAR(100) NOT NULL,
-`email` VARCHAR(150) NOT NULL,
-`phone_number` INT(11) NOT NULL,
-`birthdate` date NOT NULL,
-`localisation` VARCHAR(150) NOT NULL,
-`is_admin` bool
-)
+INSERT INTO
+    `user` (
+        `firstname`,
+        `lastname`,
+        `username`,
+        `password`,
+        `email`,
+        `phone_number`,
+        `birthdate`,
+        `localisation`,
+        `is_admin`
+    )
+VALUES (
+        'Elise',
+        'Admin',
+        'admin',
+        '$2y$10$gosLWfQZux3cZp5G69h2peX.LN4JumOqgfiKLvcksVQsfjT.7lvTa',
+        'admin@admin.com',
+        0506070809,
+        '2022-01-01',
+        'France',
+        1
+    );
 
-CREATE TABLE `contact` (
-`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`firstname` VARCHAR(100) NOT NULL,
-`lastname` VARCHAR(100) NOT NULL,
-`email` VARCHAR(150) NOT NULL,
-`message` VARCHAR(3000) NOT NULL,
-`user_id` INT NULL,
-FOREIGN KEY (user_id) REFERENCES user(id)
-)
+CREATE TABLE
+    `contact` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `firstname` VARCHAR(100) NOT NULL,
+        `lastname` VARCHAR(100) NOT NULL,
+        `email` VARCHAR(150) NOT NULL,
+        `message` VARCHAR(3000) NOT NULL,
+        `user_id` INT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id)
+    );
 
-CREATE TABLE `ad_type` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `nametype` VARCHAR(100) NOT NULL
-)
+CREATE TABLE
+    `ad_type` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `nametype` VARCHAR(100) NOT NULL
+    );
 
-CREATE TABLE `category` (
-`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`name` VARCHAR(255) NOT NULL
-)
+CREATE TABLE
+    `category` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `name` VARCHAR(255) NOT NULL
+    );
 
-CREATE TABLE `ad` (
-`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`title` VARCHAR(100) NULL,
-`image` VARCHAR(500),
-`description` VARCHAR(6000) NULL,
-`username` VARCHAR(100) NULL,
-`localisation` VARCHAR(150) NULL,
-`published_date` date,
-`user_id` INT NOT NULL,
-FOREIGN KEY (user_id) REFERENCES user(id),
-`category_id` INT NOT NULL,
-FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE,
-`ad_type_id` INT NOT NULL,
-FOREIGN KEY (ad_type_id) REFERENCES ad_type(id)
-)
+CREATE TABLE
+    `ad` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `title` VARCHAR(100) NULL,
+        `image` VARCHAR(500),
+        `description` VARCHAR(6000) NULL,
+        `username` VARCHAR(100) NULL,
+        `localisation` VARCHAR(150) NULL,
+        `published_date` date,
+        `user_id` INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id),
+        `category_id` INT NOT NULL,
+        FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE,
+        `ad_type_id` INT NOT NULL,
+        FOREIGN KEY (ad_type_id) REFERENCES ad_type(id)
+    );
 
-CREATE TABLE `image` (
-id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`image_name` VARCHAR(500),
-`ad_id` INT NOT NULL,
-FOREIGN KEY (ad_id) REFERENCES ad(id)
-)
+CREATE TABLE
+    `image` (
+        id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `image_name` VARCHAR(500),
+        `ad_id` INT NOT NULL,
+        FOREIGN KEY (ad_id) REFERENCES ad(id)
+    );
 
-CREATE TABLE `report_ad`(
-`id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`report_reason` VARCHAR(255) DEFAULT NULL,
-`ad_id` INT NULL,
-FOREIGN KEY (ad_id) REFERENCES ad(id)
-)
+CREATE TABLE
+    `report_ad`(
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `report_reason` VARCHAR(255) DEFAULT NULL,
+        `ad_id` INT NULL,
+        FOREIGN KEY (ad_id) REFERENCES ad(id)
+    );
 
-CREATE TABLE `signal_report` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `report_ad_id` INT NULL,
-    FOREIGN KEY (report_ad_id) REFERENCES report_ad(id)
-)
-  
+CREATE TABLE
+    `signal_report` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        `report_ad_id` INT NULL,
+        FOREIGN KEY (report_ad_id) REFERENCES report_ad(id)
+    );
