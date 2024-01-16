276ce2dcc53885913dda995430e72240f4da6ffd
vincentr-wcs <vincent.ravanel@wildcodeschool.com>
2024-01-16 12:09:30 +0100

    fix bug prod

--- public/assets/JS/script_loadmore.js
+++ public/assets/JS/script_loadmore.js
@@ -18,9 +18,9 @@ propositions.addEventListener('click', () => {
 })
 
 button.addEventListener('click', () => {
-const type = button.getAttribute('data-type')
-const category = document.querySelector('#category')
-    fetch(`http://localhost:8000/annonceSearch?page=${button.value}&category=${category.value}&type=${type}`)
+    const type = button.getAttribute('data-type')
+    const category = document.querySelector('#category')
+    fetch(`/annonceSearch?page=${button.value}&category=${category.value}&type=${type}`)
         .then(response => response.json())
         .then(data => {
             const currentCard = document.getElementById('newCard')
@@ -54,10 +54,10 @@ const category = document.querySelector('#category')
             </div>
         </div> `
 
-                }) 
-                
-                if (data.count[0].countads <= (button.value)*2) {                    
-                    affichageChargerPlus.style.display = "none"   
+                })
+
+                if (data.count[0].countads <= (button.value) * 2) {
+                    affichageChargerPlus.style.display = "none"
                 }
             }
 
@@ -65,46 +65,46 @@ const category = document.querySelector('#category')
         .catch(error => alert("Erreur : " + error));
 
     button.setAttribute('value', parseInt(button.value) + 1)
-        
+
 });
 
-function change_period2_laptop(period){     
+function change_period2_laptop(period) {
     let toutes = document.getElementById("toutes");
     let recherches = document.getElementById("recherches");
     let propositions = document.getElementById("propositions");
     let selector1 = document.getElementById("selector1");
-    if(period === "toutes") {       
-      selector1.style.left = 0;
-      selector1.style.width = toutes.clientWidth + "px";    
-      selector1.innerHTML = "Toutes";    
-    }else if(period === "recherches"){     
-      selector1.style.left = recherches.clientWidth + "px";
-      selector1.style.width = recherches.clientWidth + "px";
-      selector1.innerHTML = "Recherches";    
-    }else{       
-      selector1.style.left = toutes.clientWidth + recherches.clientWidth + 1 + "px";
-      selector1.style.width = toutes.clientWidth + "px";
-      selector1.innerHTML = "Propositions";    
-    }   
-  }
-  
-  function change_period2_mobile(period){    
+    if (period === "toutes") {
+        selector1.style.left = 0;
+        selector1.style.width = toutes.clientWidth + "px";
+        selector1.innerHTML = "Toutes";
+    } else if (period === "recherches") {
+        selector1.style.left = recherches.clientWidth + "px";
+        selector1.style.width = recherches.clientWidth + "px";
+        selector1.innerHTML = "Recherches";
+    } else {
+        selector1.style.left = toutes.clientWidth + recherches.clientWidth + 1 + "px";
+        selector1.style.width = toutes.clientWidth + "px";
+        selector1.innerHTML = "Propositions";
+    }
+}
+
+function change_period2_mobile(period) {
     let toutes = document.getElementById("toutes2");
     let recherches = document.getElementById("recherches2");
     let propositions = document.getElementById("propositions2");
     let selector2 = document.getElementById("selector2");
-    if(period === "toutes2"){
-      selector2.style.top = 0;
-      selector2.style.height = toutes.clientHeight + "px";
-      selector2.innerHTML = "Toutes";
-    }else if(period === "recherches2"){
-      selector2.style.top = recherches.clientHeight + "px";
-      selector2.style.height = recherches.clientHeight + "px";
-      selector2.innerHTML = "Recherches";
-    }else{
-      selector2.style.top = toutes.clientHeight + recherches.clientHeight + 1 + "px";
-      selector2.style.height = toutes.clientHeight + "px";
-      selector2.innerHTML = "Propositions";
+    if (period === "toutes2") {
+        selector2.style.top = 0;
+        selector2.style.height = toutes.clientHeight + "px";
+        selector2.innerHTML = "Toutes";
+    } else if (period === "recherches2") {
+        selector2.style.top = recherches.clientHeight + "px";
+        selector2.style.height = recherches.clientHeight + "px";
+        selector2.innerHTML = "Recherches";
+    } else {
+        selector2.style.top = toutes.clientHeight + recherches.clientHeight + 1 + "px";
+        selector2.style.height = toutes.clientHeight + "px";
+        selector2.innerHTML = "Propositions";
     }
     button.setAttribut("data-type", type);
-  }
+}
